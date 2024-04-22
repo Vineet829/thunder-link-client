8718a942df724feb8e727cbc6114fd38c3d3a445
Vineet829 <vineetsingh5987@gmail.com>
2024-04-23 03:46:32 +0530

    apply final changes

--- client/clients/api.ts
+++ client/clients/api.ts
@@ -3,8 +3,7 @@ import { GraphQLClient } from "graphql-request";
 const isClient = typeof window !== "undefined";
 
 export const graphqlClient = new GraphQLClient(
-  process.env.NEXT_PUBLIC_API_URL as string,
-  {
+  "https://d213k0kfmzs5wg.cloudfront.net/graphql",  {
     headers: () => ({
       Authorization: isClient
         ? `Bearer ${window.localStorage.getItem("__thunder_token")}`
