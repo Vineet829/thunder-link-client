b3bcc2eb3ed5d83b67a42653c2a8b7c1e32d3d22
Vineet829 <vineetsingh5987@gmail.com>
2024-04-23 03:22:38 +0530

    apply final changes

--- client/clients/api.ts
+++ client/clients/api.ts
@@ -3,7 +3,7 @@ import { GraphQLClient } from "graphql-request";
 const isClient = typeof window !== "undefined";
 
 export const graphqlClient = new GraphQLClient(
-  "https://d213k0kfmzs5wg.cloudfront.net",
+  process.env.NEXT_PUBLIC_API_URL as string,
   {
     headers: () => ({
       Authorization: isClient
